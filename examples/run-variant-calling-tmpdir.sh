de74426ed812269bc31c8ad97170471e9cbf9091
Maithili Kalamkar <maithili86@gmail.com>
2019-08-20 10:41:09 +0200

    Create run-variant-calling-tmpdir.sh

---
+++ examples/run-variant-calling-tmpdir.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+set -x
+ecolipath=$PWD
+
+mkdir -p data/ref_genome
+cp /project/surfadvisors/Data/ecoli-analysis/data/ref_genome/ecoli_rel606.fasta data/ref_genome/
+ls data/ref_genome
+
+mkdir data/trimmed_fastq_small
+cp /project/surfadvisors/Data/ecoli-analysis/data/trimmed_fastq_small/*fastq data/trimmed_fastq_small/
+ls data/trimmed_fastq_small
+
+mkdir results
+cd $ecolipath/results
+
+genome=$ecolipath/data/ref_genome/ecoli_rel606.fasta
+
+bwa index $genome
+
+mkdir -p sam bam bcf vcf
+
+for fq1 in $ecolipath/data/trimmed_fastq_small/*_1.trim.sub.fastq
+    do
+    echo "working with file $fq1"
+
+    base=$(basename $fq1 _1.trim.sub.fastq)
+    echo "base name is $base"
+
+    fq1=$ecolipath/data/trimmed_fastq_small/${base}_1.trim.sub.fastq
+    fq2=$ecolipath/data/trimmed_fastq_small/${base}_2.trim.sub.fastq
+    sam=$ecolipath/results/sam/${base}.aligned.sam
+    bam=$ecolipath/results/bam/${base}.aligned.bam
+    sorted_bam=$ecolipath/results/bam/${base}.aligned.sorted.bam
+    raw_bcf=$ecolipath/results/bcf/${base}_raw.bcf
+    variants=$ecolipath/results/bcf/${base}_variants.vcf
+    final_variants=$ecolipath/results/vcf/${base}_final_variants.vcf 
+
+    bwa mem $genome $fq1 $fq2 > $sam
+    samtools view -S -b $sam > $bam
+    samtools sort -o $sorted_bam $bam 
+    samtools index $sorted_bam
+    bcftools mpileup -O b -o $raw_bcf -f $genome $sorted_bam
+    bcftools call --ploidy 1 -m -v -o $variants $raw_bcf 
+    vcfutils.pl varFilter $variants > $final_variants
+   
+    done
+
+cp -r $TMPDIR/var-calling/results $HOME/ecoli-analysis-tmpdir/
